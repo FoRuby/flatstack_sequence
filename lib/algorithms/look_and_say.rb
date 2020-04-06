e2beb1541980a9ce01d3125aecc7991f167256d3
FoRuby <aggabitov@yandex.ru>
2020-04-06 16:04:28 +0300

    LAS

--- lib/algorithms/look_and_say.rb
+++ lib/algorithms/look_and_say.rb
@@ -8,8 +8,18 @@ class LookAndSay
   def self.call(elements)
     validate_sequence!(elements: elements, regexp: REGEXP, min_length: 1)
 
-    elements.last.chars.chunk(&:itself)
-            .map { |element, arr| [arr.size, element] }
-            .join
+    string = elements.last.chars
+    count = 1
+    output = ''
+    string.each.with_index do |char, i|
+      if char == string[i + 1]
+        count += 1
+      else
+        output += count.to_s + char
+        count = 1
+      end
+    end
+
+    output
   end
 end
